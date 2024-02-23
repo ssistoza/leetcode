d58650b762eeb166a24deac8f8ae31030736716e
Shane Avery Sistoza <me@ssistoza.dev>
2024-02-23 16:11:20 -0500

    feat: rename

--- src/longest-common-prefix.spec.ts
+++ src/longest-common-prefix.spec.ts
@@ -1,28 +1,12 @@
 import { describe, test, expect } from 'vitest';
+import { longestCommonPrefix } from './longest-common-prefix.js';
 
-function longestCommonPrefix(strs: string[]): string {
-  const [first, ...rest] = strs;
-
-  let last = 1;
-  while (last <= first!.length) {
-    for (const str of rest) {
-      const prefix = first!.slice(0, last);
-      const strPrefix = str.slice(0, last);
-      if (prefix !== strPrefix) return first!.slice(0, last - 1);
-    }
-
-    last++;
-  }
-
-  return first!;
-}
-
-describe('longestCommonPrefix', () => {
+describe.each(longestCommonPrefix)('longestCommonPrefix %s', (_, fn) => {
   test('["flower","flow","flight"]', () => {
-    expect(longestCommonPrefix(['flower', 'flow', 'flight'])).toBe('fl');
+    expect(fn(['flower', 'flow', 'flight'])).toBe('fl');
   });
 
   test('["dog","racecar","car"]', () => {
-    expect(longestCommonPrefix(['dog', 'racecar', 'car'])).toBe('');
+    expect(fn(['dog', 'racecar', 'car'])).toBe('');
   });
 });
---
+++ src/longest-common-prefix.ts
@@ -0,0 +1,20 @@
+const _longestCommonPrefix: Record<string, (strs: string[]) => string> = {
+  v1(strs) {
+    const [first, ...rest] = strs;
+
+    let last = 1;
+    while (last <= first!.length) {
+      for (const str of rest) {
+        const prefix = first!.slice(0, last);
+        const strPrefix = str.slice(0, last);
+        if (prefix !== strPrefix) return first!.slice(0, last - 1);
+      }
+
+      last++;
+    }
+
+    return first!;
+  },
+};
+
+export const longestCommonPrefix = Object.entries(_longestCommonPrefix);
