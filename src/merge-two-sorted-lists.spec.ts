3be0bd193d5a7192d3e27225afc4cad615dc9bfe
Shane Avery Sistoza <me@ssistoza.dev>
2024-02-24 17:28:20 -0500

    fix: lists

--- src/merge-two-sorted-lists.spec.ts
+++ src/merge-two-sorted-lists.spec.ts
@@ -7,4 +7,10 @@ describe.each(mergeTwoLists)('mergeTwoLists %s', (_, fn) => {
       fn(ListNode.construct([1, 2, 4]), ListNode.construct([1, 3, 4]))
     ).toEqual(ListNode.construct([1, 1, 2, 3, 4, 4]));
   });
+
+  test('[], [0]', () => {
+    expect(fn(ListNode.construct([]), ListNode.construct([0]))).toEqual(
+      ListNode.construct([0])
+    );
+  });
 });
